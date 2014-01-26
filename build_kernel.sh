0ce2fd8a368976848f8a231f09ef133a6f8377fc
Jeongdeokho <atti.alive@icloud.com>
2014-01-26 17:58:58 +0900

    set for build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+kdir=`readlink -f .`
+cd $kdir
+cd ..
+home=`readlink -f .`
+ramdisk=$home/ramdisk_ics/ug28 # UG28
+toolchain=$home/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+version=minah
+defconfig_name=minah_defconfig
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 export USE_SEC_FIPS_MODE=true
-
-make u1_kor_lgt_defconfig
-make
+export CROSS_COMPILE=$toolchain
+cd $kdir
+mkdir out
+rm -rf out/*
+rm -rf ./pack/stock/boot/zImage
+mv .git git
+make $defconfig_name
+make mrproper
+make $defconfig_name
+rm -rf $ramdisk/lib/modules/*
+make -j16 CONFIG_INITRAMFS_SOURCE="$ramdisk"
+find . -name "*.ko" -exec mv {} $ramdisk/lib/modules/ \;
+make clean
+make -j16 CONFIG_INITRAMFS_SOURCE="$ramdisk"
+cp ./arch/arm/boot/zImage ./pack/stock/boot/
+cd $kdir/pack/stock
+zip -r [$version].$(date -u +%m)-$(date -u +%d)-$(date -u +%s).zip ./
+mv ./*.zip $kdir/out/
+cd $kdir
+make mrproper
+mv git .git
