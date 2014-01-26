0a84933c8d096d011d3f8356a09b423e923866fc
Jeongdeokho <atti.alive@icloud.com>
2014-01-26 19:30:23 +0900

    dualboot feature

---
+++ dual.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+kdir=`readlink -f .`
+cd $kdir
+cd ..
+home=`readlink -f .`
+#ramdisk=$home/ramdisk_ics/ug28 # UG28
+ramdisk=$home/ramdisk_ics/dualboot # UG28
+toolchain=$home/toolchain/linaro_4.8/bin/arm-linux-gnueabihf-
+version=Dualboot
+defconfig_name=minah_defconfig
+
+export ARCH=arm
+export USE_SEC_FIPS_MODE=true
+export CROSS_COMPILE=$toolchain
+cd $kdir
+mkdir out
+rm -rf out/*
+rm -rf ./pack/dualboot/boot/zImage
+mv .git git
+make $defconfig_name
+make mrproper
+make $defconfig_name
+rm -rf $ramdisk/lib/modules/*
+make -j16 CONFIG_INITRAMFS_SOURCE="$ramdisk"
+find . -name "*.ko" -exec mv {} $ramdisk/lib/modules/ \;
+make clean
+make -j16 CONFIG_INITRAMFS_SOURCE="$ramdisk"
+cp ./arch/arm/boot/zImage ./pack/dualboot/boot/
+cd $kdir/pack/dualboot
+zip -r [$version.$(date -u +%m)-$(date -u +%d)-$(date -u +%s)].zip ./
+mv ./*.zip $kdir/out/
+cd $kdir
+make mrproper
+mv git .git
